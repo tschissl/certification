d915dd00a181bc5c310875e6af7cf741a7a3bad2
tschissl <thomas.meisel@unileoben.ac.at>
2014-06-18 13:14:56 +0200

    corrections made

--- GOMGather1.R
+++ GOMGather1.R
@@ -4,8 +4,8 @@
 # Thomas Meisel
 ####################
 
-GAS <- read.csv("~/GitHub/certification/GAS.csv", sep=";")
-OKUM <- read.csv("~/GitHub/certification/OKUM.csv", sep=";")
-MUH <- read.csv("~/GitHub/certification/MUH.csv", sep=";")
-OKUM.methods <- read.csv("~/GitHub/certification/OKUM methods.csv", sep=";")
-OKUM.measurand <- read.csv("~/GitHub/certification/OKUM measurand.csv", sep=";")
+GAS <- read.csv("~/GitHub/certification/data/GAS.csv", sep=";")
+OKUM <- read.csv("~/GitHub/certification/data/OKUM.csv", sep=";")
+MUH <- read.csv("~/GitHub/certification/data/MUH.csv", sep=";")
+OKUM.methods <- read.csv("~/GitHub/certification/data/OKUM methods.csv", sep=";")
+OKUM.outlier <- read.csv("~/GitHub/certification/data/OKUM.outlier.csv", sep=";")
